92422e44fc9ae45da06ac174166f6686029eee5d
panwenbin <panwenbin@gmail.com>
2025-03-19 22:56:35 +0800

    平铺浮动改中文

--- config.h
+++ config.h
@@ -40,8 +40,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "平铺",      tile },    /* first entry is default */
+	{ "浮动",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
